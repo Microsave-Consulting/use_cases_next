8a90618502fcecd8bc29369edbca30488b9207cb
Suhird Singh <suhird.singh@microsave.net>
2026-03-18 15:25:31 +0530

    Deployment updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/use-cases-next',   // e.g. '/use-cases-next'
+  basePath: 'digitalidinnovations.com/use-cases-next',   // e.g. '/use-cases-next'
   images: {
     unoptimized: true,           // already set — required for static export
   },
